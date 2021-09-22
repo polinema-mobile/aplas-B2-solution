06c314d186b44b81ae24b134c26497482ca70bc8
AikaMinami <aikatan.aikachan@gmail.com>
2021-09-22 15:30:42 +0700

    Task 04

--- app/src/main/java/org/aplas/colorgamex/MyActivity.java
+++ app/src/main/java/org/aplas/colorgamex/MyActivity.java
@@ -4,16 +4,68 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MyActivity extends AppCompatActivity {
+
+    TextView timer;
+    TextView clrText;
+    TextView scoreText;
+    EditText passwd;
+    Button submit;
+    Button start;
+    ViewGroup accessbox;
+    ViewGroup colorbox;
+    ViewGroup buttonbox1;
+    ViewGroup buttonbox2;
+    ViewGroup scorebox;
+    ViewGroup progressbox;
+    ProgressBar progress;
+    Switch isMinus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_layout);
+
+        timer = (TextView)findViewById(R.id.timerText);
+        clrText = (TextView)findViewById(R.id.clrText);
+        scoreText = (TextView)findViewById(R.id.scoreText);
+        passwd = (EditText)findViewById(R.id.appCode);
+        submit = (Button)findViewById(R.id.submitBtn);
+        start = (Button)findViewById(R.id.startBtn);
+        accessbox = (ViewGroup)findViewById(R.id.accessBox);
+        colorbox = (ViewGroup)findViewById(R.id.colorBox);
+        buttonbox1 = (ViewGroup)findViewById(R.id.buttonBox1);
+        buttonbox2 = (ViewGroup)findViewById(R.id.buttonBox2);
+        scorebox = (ViewGroup)findViewById(R.id.scoreBox);
+        progressbox = (ViewGroup)findViewById(R.id.progressBox);
+        progress = (ProgressBar)findViewById(R.id.progressScore);
+        isMinus = (Switch)findViewById(R.id.isMinus);
     }
 
     public void openGame(View v) {
+        String keyword = getString(R.string.keyword);
+        String pass = passwd.getText().toString();
+        if(pass.equals(keyword)){
+            passwd.setVisibility(View.INVISIBLE);
+            submit.setVisibility(View.INVISIBLE);
+            accessbox.setVisibility(View.VISIBLE);
+            colorbox.setVisibility(View.VISIBLE);
+            buttonbox1.setVisibility(View.VISIBLE);
+            buttonbox2.setVisibility(View.VISIBLE);
+            scorebox.setVisibility(View.VISIBLE);
+            progressbox.setVisibility(View.VISIBLE);
+            Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Password is wrong", Toast.LENGTH_LONG).show();
+        }
     }
     public void startGame(View v) {
     }
